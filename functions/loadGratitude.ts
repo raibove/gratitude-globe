46196a6bb5eb8471a79fd23211626804953186ac
Shweta kale <shwetakale144@gmail.com>
2024-05-13 12:18:52 +0530

    add lambda to all function

--- functions/loadGratitude.ts
+++ functions/loadGratitude.ts
@@ -1,19 +1,20 @@
-import { getStore } from "@netlify/blobs";
+import { connectLambda, getStore } from "@netlify/blobs";
 
 
 export async function handler(req, context) {
+    connectLambda(req)
     const noteStore = getStore('note');
     const id = req.queryStringParameters.id;
     try {
         let noteData = await noteStore.get(id, { type: 'json' });
-        return {
-            statusCode: 200,
-            body: JSON.stringify(noteData),
-            headers: {
-                'Cache-Control': 'public, max-age=80000',
-                'Netlify-Vary': 'query',
-            }
-        };
+            return {
+                statusCode: 200,
+                body: JSON.stringify(noteData),
+                headers: {
+                    'Cache-Control': 'public, max-age=80000',
+                    'Netlify-Vary': 'query',
+                }
+            };
     } catch (e) {
         console.log(e)
         return {
